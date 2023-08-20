9180162a5e90f31ec5b17105fc7812b1070465fc
jsmile <jsmile@naver.com>
2023-08-20 22:48:40 +0900

    basic submit of TextFormField() & Form

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:open_weather_cubit/cubits/weather/weather_cubit.dart';
+import 'package:open_weather_cubit/pages/search_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,16 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-    _fetchWeather();
-  }
-
-  // WeatherRepository 테스트
-  void _fetchWeather() {
-    context.read<WeatherCubit>().fetchWeather('london');
-  }
+  // 사용자가 navigator back 을 하면 null 이 될 수 있으므로 nullable 선언.
+  String? city;
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +21,20 @@ class _HomePageState extends State<HomePage> {
           'Weather',
           style: TextStyle(color: Colors.white),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () async {
+              // 검색 결과를 받는데 시간이 걸리므로 비동기 처리.
+              city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SearchPage(),
+                ),
+              );
+            },
+          )
+        ],
       ),
       body: const Center(
         child: Text('Home'),
